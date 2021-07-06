ba42cc4979e806a7682f9696d9749a0a62525b51
Nayanajith Bandaranayake <hbmnbandaranayake9988033@gmail.com>
2021-07-06 18:07:59 +0530

    Fixed responsive bugs

--- src/components/Dictionary.tsx
+++ src/components/Dictionary.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import SearchField from "./SearchField";
 import ResultContainer from "./ResultContainer";
-import { useDictionaryContext } from "../context/DictionaryContext";
 
 const Dictionary = () => {
-  const value = useDictionaryContext()!;
-
   return (
     <section className="dictionary">
       <SearchField />
